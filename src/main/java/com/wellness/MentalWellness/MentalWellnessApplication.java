b41ac940969d2fef8b5901d01ff1814846ab600e
Gaurav Goyal <goyalgaurav699@gmail.com>
2025-05-31 18:53:31 +0530

    initial commit

---
+++ src/main/java/com/wellness/MentalWellness/MentalWellnessApplication.java
@@ -0,0 +1,13 @@
+package com.wellness.MentalWellness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentalWellnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentalWellnessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wellness/MentalWellness/MentalWellnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellness.MentalWellness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentalWellnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
